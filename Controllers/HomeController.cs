2c235f9d07d407ed4d80d733235a339582a2fa0b
MartinZilber <martin.zilber.2008@gmail.com>
2024-08-08 13:31:06 -0300

    Función créditos

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Creditos()
+    {
+        return View("creditos");
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
