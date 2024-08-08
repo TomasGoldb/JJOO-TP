261faf5e6e12e271e72909eaa4bc54e30a5ec6db
MATI EZEQUIEL GRZENDA <48716278@ort>
2024-08-08 13:37:45 -0300

    Corregir Deportista.cs (no es estática)

--- Models/Deportista.cs
+++ Models/Deportista.cs
@@ -1,10 +1,10 @@
-static class Deportista
+class Deportista
 {
-    public static int IdDeportista {get; set;}
-    public static string Apellido {get; set;}
-    public static string Nombre {get; set;}
-    public static DateTime FechaNacimiento {get; set;}
-    public static string Foto {get; set;}
-    public static int IdPais {get; set;}
-    public static int IdDeporte {get; set;}
+    public int IdDeportista {get; set;}
+    public string Apellido {get; set;}
+    public string Nombre {get; set;}
+    public DateTime FechaNacimiento {get; set;}
+    public string Foto {get; set;}
+    public int IdPais {get; set;}
+    public int IdDeporte {get; set;}
 }
