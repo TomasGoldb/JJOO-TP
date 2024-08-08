6c6f7c69d2efee5aa737969a1d397b643b7992b2
MATI EZEQUIEL GRZENDA <48716278@ort>
2024-08-08 13:36:29 -0300

    Clase Deportista.cs

---
+++ Models/Deportista.cs
@@ -0,0 +1,10 @@
+static class Deportista
+{
+    public static int IdDeportista {get; set;}
+    public static string Apellido {get; set;}
+    public static string Nombre {get; set;}
+    public static DateTime FechaNacimiento {get; set;}
+    public static string Foto {get; set;}
+    public static int IdPais {get; set;}
+    public static int IdDeporte {get; set;}
+}
