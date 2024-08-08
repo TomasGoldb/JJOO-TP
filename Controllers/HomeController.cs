5f75bcce3a217d56caafe7bb45fc2a0f57a02fb6
MartinZilber <martin.zilber.2008@gmail.com>
2024-08-08 13:50:46 -0300

    Algunas cosas

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -46,7 +46,6 @@ public class HomeController : Controller
     }
     public IActionResult VerDetalleDeportista(int idDeportista)
     {
-        
         return View("detalledeportista");
     }
 
