70d8e1dd2bf86b1b997230e709c6d8acbb912459
MartinZilber <martin.zilber.2008@gmail.com>
2024-08-08 14:10:02 -0300

    AgregarDeportista - Alpha 1.0

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -35,7 +35,6 @@ public class HomeController : Controller
     {
         return View("deportes");
     }
-
     public IActionResult AgregarDeportista()
     {
         return View("agregardeportista");//CONTINUAR
