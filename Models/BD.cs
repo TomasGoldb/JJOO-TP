d0f161353f2ee376269627130aebdcba82860525
Android Studio <devandroid@ort>
2024-08-14 10:14:18 -0300

    bd

---
+++ Models/BD.cs
@@ -0,0 +1,36 @@
+using System.Data.SqlClient;
+using Dapper;
+
+namespace JJOO_TP.Models{
+public class BD
+{
+   private static string _connectionString {get;set;} = @"Server=localhost;DataBase=IsTravel;Trusted_Connection=true;";
+
+
+    public static List<Usuario> Seleccionar(string sql){
+        List<Usuario> listaUsuario = new List<Usuario>();
+        using(SqlConnection db = new SqlConnection(_connectionString)){
+            listaUsuario = db.Query<Usuario>(sql).ToList();
+        }
+        return listaUsuario;
+    }
+
+
+    public static void CrearUsuario(Usuario objeto){
+            using(SqlConnection db = new SqlConnection(_connectionString)){
+            string sql = $"INSERT INTO Usuario(Nombre,Nick,Contrasena,Mail) VALUES('{objeto.Nombre}','{objeto.Nick}','{objeto.GetContrasena()}','{objeto.GetMail()}')";
+            db.Execute(sql, new{Nombre = objeto.Nombre, Nick = objeto.Nick, Contrasena = objeto.GetContrasena(), Mail=objeto.GetMail()});
+        }
+        }
+
+    public static void UpdateFotoPerfil(Usuario objeto){
+            using(SqlConnection db = new SqlConnection(_connectionString)){
+            string sql = $"UPDATE usuario SET FotoPerfil='{objeto.FotoPerfil}' where mail='{objeto.GetMail()}'";
+            db.Execute(sql);
+        }
+        }
+        
+
+    
+}
+}
