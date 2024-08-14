896e6b0e4f16a082595769a3e0ccb383dc184fe2
Android Studio <devandroid@ort>
2024-08-14 12:13:30 -0300

    antes de cierre

---
+++ insertar banderitas.sql
@@ -0,0 +1,117 @@
+INSERT INTO Pais (Nombre, Bandera, FechaFundacion)
+VALUES 
+('Afghanistan', 'https://flagcdn.com/w320/af.png', '1747-10-08'),
+('Albania', 'https://flagcdn.com/w320/al.png', '1912-11-28'),
+('Algeria', 'https://flagcdn.com/w320/dz.png', '1962-07-05'),
+('Andorra', 'https://flagcdn.com/w320/ad.png', '1278-09-08'),
+('Angola', 'https://flagcdn.com/w320/ao.png', '1975-11-11'),
+('Antigua and Barbuda', 'https://flagcdn.com/w320/ag.png', '1981-11-01'),
+('Argentina', 'https://flagcdn.com/w320/ar.png', '1816-07-09'),
+('Armenia', 'https://flagcdn.com/w320/am.png', '1918-05-28'),
+('Australia', 'https://flagcdn.com/w320/au.png', '1901-01-01'),
+('Austria', 'https://flagcdn.com/w320/at.png', '1955-07-27');
+
+-- Insert 2
+INSERT INTO Pais (Nombre, Bandera, FechaFundacion)
+VALUES 
+('Azerbaijan', 'https://flagcdn.com/w320/az.png', '1991-08-30'),
+('Bahamas', 'https://flagcdn.com/w320/bs.png', '1973-07-10'),
+('Bahrain', 'https://flagcdn.com/w320/bh.png', '1971-08-15'),
+('Bangladesh', 'https://flagcdn.com/w320/bd.png', '1971-12-16'),
+('Barbados', 'https://flagcdn.com/w320/bb.png', '1966-11-30'),
+('Belarus', 'https://flagcdn.com/w320/by.png', '1991-08-25'),
+('Belgium', 'https://flagcdn.com/w320/be.png', '1830-10-04'),
+('Belize', 'https://flagcdn.com/w320/bz.png', '1981-09-21'),
+('Benin', 'https://flagcdn.com/w320/bj.png', '1960-08-01'),
+('Bhutan', 'https://flagcdn.com/w320/bt.png', '1907-12-17');
+
+-- Insert 3
+INSERT INTO Pais (Nombre, Bandera, FechaFundacion)
+VALUES 
+('Bolivia', 'https://flagcdn.com/w320/bo.png', '1825-08-06'),
+('Bosnia and Herzegovina', 'https://flagcdn.com/w320/ba.png', '1992-03-01'),
+('Botswana', 'https://flagcdn.com/w320/bw.png', '1966-09-30'),
+('Brazil', 'https://flagcdn.com/w320/br.png', '1822-09-07'),
+('Brunei', 'https://flagcdn.com/w320/bn.png', '1984-01-01'),
+('Bulgaria', 'https://flagcdn.com/w320/bg.png', '1908-09-22'),
+('Burkina Faso', 'https://flagcdn.com/w320/bf.png', '1960-08-05'),
+('Burundi', 'https://flagcdn.com/w320/bi.png', '1962-07-01'),
+('Cabo Verde', 'https://flagcdn.com/w320/cv.png', '1975-07-05'),
+('Cambodia', 'https://flagcdn.com/w320/kh.png', '1953-11-09');
+
+-- Insert 4
+INSERT INTO Pais (Nombre, Bandera, FechaFundacion)
+VALUES 
+('Cameroon', 'https://flagcdn.com/w320/cm.png', '1960-01-01'),
+('Canada', 'https://flagcdn.com/w320/ca.png', '1867-07-01'),
+('Central African Republic', 'https://flagcdn.com/w320/cf.png', '1960-08-13'),
+('Chad', 'https://flagcdn.com/w320/td.png', '1960-08-11'),
+('Chile', 'https://flagcdn.com/w320/cl.png', '1818-02-12'),
+('China', 'https://flagcdn.com/w320/cn.png', '1949-10-01'),
+('Colombia', 'https://flagcdn.com/w320/co.png', '1810-07-20'),
+('Comoros', 'https://flagcdn.com/w320/km.png', '1975-07-06'),
+('Congo', 'https://flagcdn.com/w320/cg.png', '1960-08-15'),
+('Costa Rica', 'https://flagcdn.com/w320/cr.png', '1821-09-15');
+
+-- Insert 5
+INSERT INTO Pais (Nombre, Bandera, FechaFundacion)
+VALUES 
+('Croatia', 'https://flagcdn.com/w320/hr.png', '1991-06-25'),
+('Cuba', 'https://flagcdn.com/w320/cu.png', '1902-05-20'),
+('Cyprus', 'https://flagcdn.com/w320/cy.png', '1960-08-16'),
+('Czech Republic', 'https://flagcdn.com/w320/cz.png', '1993-01-01'),
+('Democratic Republic of the Congo', 'https://flagcdn.com/w320/cd.png', '1960-06-30'),
+('Denmark', 'https://flagcdn.com/w320/dk.png', '1849-06-05'),
+('Djibouti', 'https://flagcdn.com/w320/dj.png', '1977-06-27'),
+('Dominica', 'https://flagcdn.com/w320/dm.png', '1978-11-03'),
+('Dominican Republic', 'https://flagcdn.com/w320/do.png', '1844-02-27'),
+('East Timor', 'https://flagcdn.com/w320/tl.png', '2002-05-20');
+
+-- Insert 6
+INSERT INTO Pais (Nombre, Bandera, FechaFundacion)
+VALUES 
+('Ecuador', 'https://flagcdn.com/w320/ec.png', '1822-05-24'),
+('Egypt', 'https://flagcdn.com/w320/eg.png', '1952-07-23'),
+('El Salvador', 'https://flagcdn.com/w320/sv.png', '1821-09-15'),
+('Equatorial Guinea', 'https://flagcdn.com/w320/gq.png', '1968-10-12'),
+('Eritrea', 'https://flagcdn.com/w320/er.png', '1993-05-24'),
+('Estonia', 'https://flagcdn.com/w320/ee.png', '1991-08-20'),
+('Eswatini', 'https://flagcdn.com/w320/sz.png', '1968-09-06'),
+('Ethiopia', 'https://flagcdn.com/w320/et.png', '1991-05-28'),
+('Fiji', 'https://flagcdn.com/w320/fj.png', '1970-10-10'),
+('Finland', 'https://flagcdn.com/w320/fi.png', '1917-12-06');
+
+-- Insert 7
+INSERT INTO Pais (Nombre, Bandera, FechaFundacion)
+VALUES 
+('France', 'https://flagcdn.com/w320/fr.png', '1789-07-14'),
+('Gabon', 'https://flagcdn.com/w320/ga.png', '1960-08-16'),
+('Gambia', 'https://flagcdn.com/w320/gm.png', '1965-02-18'),
+('Georgia', 'https://flagcdn.com/w320/ge.png', '1991-04-09'),
+('Germany', 'https://flagcdn.com/w320/de.png', '1990-10-03'),
+('Ghana', 'https://flagcdn.com/w320/gh.png', '1957-03-06'),
+('Greece', 'https://flagcdn.com/w320/gr.png', '1821-03-25'),
+('Grenada', 'https://flagcdn.com/w320/gd.png', '1974-02-07'),
+('Guatemala', 'https://flagcdn.com/w320/gt.png', '1821-09-15'),
+('Guinea', 'https://flagcdn.com/w320/gn.png', '1958-10-02');
+
+-- Insert 8
+INSERT INTO Pais (Nombre, Bandera, FechaFundacion)
+VALUES 
+('Guinea-Bissau', 'https://flagcdn.com/w320/gw.png', '1973-09-24'),
+('Guyana', 'https://flagcdn.com/w320/gy.png', '1966-05-26'),
+('Haiti', 'https://flagcdn.com/w320/ht.png', '1804-01-01'),
+('Honduras', 'https://flagcdn.com/w320/hn.png', '1821-09-15'),
+('Hungary', 'https://flagcdn.com/w320/hu.png', '1918-11-16'),
+('Iceland', 'https://flagcdn.com/w320/is.png', '1944-06-17'),
+('India', 'https://flagcdn.com/w320/in.png', '1947-08-15')
+
+
+
+
+
+
+delete from pais
+select * from pais
+
+
